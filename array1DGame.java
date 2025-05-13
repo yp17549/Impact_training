670b7a71b8c40a2d6ce37af23d15d4ffa6b72ada
yp17549 <yashpatil17549@gmail.com>
2025-05-13 14:19:46 +0530

    Create array1DGame.java

---
+++ array1DGame.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class array1DGame {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+
+        }
+    }
+}
