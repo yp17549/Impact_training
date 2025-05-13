1d916a9b99ac8b956cc0bbfec0475f742539a733
yp17549 <yashpatil17549@gmail.com>
2025-05-13 14:09:54 +0530

    Update heightandweightproblem.java

--- heightandweightproblem.java
+++ heightandweightproblem.java
@@ -6,11 +6,14 @@ public class heightandweightproblem {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int a[] = new int[n * 2];
+        for (int i = 0; i < n * 2; i++) {
+            a[i] = sc.nextInt();
+        }
         int max_h = 0;
         int max_w = 0;
         int max_h_i = 0;
         int max_w_i = 0;
-        int a[] = new int[n * 2];
         for (int i = 0; i < n * 2; i++) {
             if (i % 2 == 0) {
                 if (a[i] > max_h) {
@@ -26,7 +29,7 @@ public class heightandweightproblem {
         }
         System.out.println("maximum height: " + max_h);
         System.out.println("maximum weight: " + max_w);
-        System.out.println("Person with maximum height: " + max_h_i);
-        System.out.println("Person with maximun weight: " + max_w_i);
+        System.out.println("Person with maximum height: " + ((max_h_i / 2) + 1));
+        System.out.println("Person with maximun weight: " + ((max_w_i / 2) + 1));
     }
 }
