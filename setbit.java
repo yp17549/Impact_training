aad3c9ca5b264b638213fcc1701fe576c24e7239
yp17549 <yashpatil17549@gmail.com>
2025-05-19 09:44:34 +0530

    Update setbit.java

--- setbit.java
+++ setbit.java
@@ -7,6 +7,15 @@ public class setbit {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-
+        while (b > 0) {
+            a = a / 10;
+            b--;
+        }
+        a = a % 10;
+        if (a == 1) {
+            System.out.println(a + "Is a Set bit.");
+        } else {
+            System.out.println("It is not a set bit.");
+        }
     }
 }
