d8bdc9c907b372d696a02add597cbdcdc88dee47
yp17549 <yashpatil17549@gmail.com>
2025-05-21 07:47:48 +0530

    q

--- palindrome.java
+++ palindrome.java
@@ -5,15 +5,20 @@ public class palindrome {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the string: ");
-        String s = sc.next();
-        int n = s.length();
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (c == s.charAt(i)) {
-                count++;
-            }
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
         }
-        System.out.println(count);
+
+        if (str.equalsIgnoreCase(reversed)) {
+            System.out.println("Yes! It's a palindrome.");
+        } else {
+            System.out.println("Nope! Not a palindrome.");
+        }
     }
 }
